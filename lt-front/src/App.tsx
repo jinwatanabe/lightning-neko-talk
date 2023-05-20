74ab64c31a8669b45afd21a129d1b16433cf4438
taiti09 <taitiabe1997@gmail.com>
2023-05-20 21:23:50 +0900

    init react app

---
+++ lt-front/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  )
+  
+}
+
+export default App
---
+++ lt-front/src/__test__/system.spec.ts
@@ -0,0 +1,5 @@
+describe("system", () => {
+	test("テストが動くか確認", () => {
+        expect(2).toEqual(1+1)
+    })
+})
